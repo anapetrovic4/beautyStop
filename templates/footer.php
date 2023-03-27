4cc09fbcc47f5ef2c24f73494dddc74ce45595ce
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-03-27 22:58:22 +0200

    Salon lepote footer

---
+++ templates/footer.php
@@ -0,0 +1,26 @@
+<style>
+footer {
+  padding: 5px;
+  margin-top: 90px;
+  color: "pink darken-3";
+}
+
+footer p {
+  margin: 5px 10px;
+  font-size: 14px;
+  display: inline-block;
+}
+
+footer .center {
+  text-align: center;
+}
+</style>
+<footer>
+  <div class="center">
+    <p>&copy; 2023 Salon lepote</p>
+    <p>Adresa: Neznanih junaka 1, Beograd</p>
+    <p>Telefon: 011 123 456</p>
+    <p>Email: info@salonlepote.com</p>
+  </div>
+</footer>
+
